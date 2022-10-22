2723ba2ee02a8fc7b0cc0c892136e74a2cd8237e
zywang1 <zywang1@uci.edu>
2022-10-22 13:39:25 -0700

    Updated index.js and app.jsx. Updated fonts.

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+const rootContainer = document.getElementById('root')
+if (rootContainer !== null) {
+    ReactDOM.createRoot(rootContainer).render(<App />)
+}
+
