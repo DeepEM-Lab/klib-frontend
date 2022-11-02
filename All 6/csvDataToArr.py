3584613c5e0e0661fc6b759a3aff09d112578d79
HikawaRina <jianqz666@gmail.com>
2022-11-02 13:27:26 -0700

    draw graphs

--- All 6/csvDataToArr.py
+++ All 6/csvDataToArr.py
@@ -1,13 +1,10 @@
 import re
 
-#scipt to transfer csv data to x-axis and y-axis arrays
+#scipt to convert csv data to data arrays
 with open("./LaMnO3.csv") as f:
     a = []
-    b = []
     for line in f:
         str1 = re.split("[,\n]", line)
-        a.append(float(str1[0]))
-        b.append(float(str1[1]))
+        a.append({'x': float(str1[0]), 'y': float(str1[1])})
 
     print(a, len(a))
-    print(b, len(b))
