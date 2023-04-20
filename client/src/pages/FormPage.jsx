e61e3662a7519bf7cd18c83cf23d8d0c683bb5e9
Guanchen Wu <guanchen2002@gmail.com>
2023-04-20 16:48:01 -0700

    Form Page changes
    - "Microscope Acquisition Details" section layout complete.

--- client/src/pages/FormPage.jsx
+++ client/src/pages/FormPage.jsx
@@ -1,8 +1,8 @@
-import { Box, Button, Grid, MenuItem, Paper, Select, Typography} from "@mui/material"
+import { Box, Button, FormControl, InputLabel, Grid, MenuItem, Paper, Select, TextField, InputAdornment, Typography} from "@mui/material"
 import { useEffect, useState } from "react";
 import PeriodicTable from "../components/PeriodicTable/PeriodicTable"
 import PageParent from "../structures/PageParent"
-import Papa from 'papaparse';
+// import Papa from 'papaparse';
 
 //notes: can only submit info in "About You" and "Spectrum Description" fieldset
 const FormPage = () => {
@@ -112,7 +112,7 @@ const FormPage = () => {
                         <span><strong>Note:</strong> An account will be created for you if you're not already registered.</span>
                     </div>
                 </div>
-            </fieldset>
+            	</fieldset>
              
 
             <fieldset>
@@ -339,98 +339,207 @@ const FormPage = () => {
     </div>} <Button onClick={() => handleToggle()}>Add Spectra Edge</Button>
 		</fieldset> */}
 
-		<fieldset>
+<fieldset>
 			<legend>Microscope Acquisition Details</legend>
 			{/* <input type="hidden" id="eelsdb_spectra_upload_nonce" name="eelsdb_spectra_upload_nonce" value="86deac9ca7" /><input type="hidden" name="_wp_http_referer" value="/submit-data/" />			 */}
 			<div>
-				<label>Microscope Name/Model</label>
-				<div>
-					<input/>
-				</div>
+				<TextField
+					label="Microscope Name/Model"
+					id=""
+					sx={{ m: 1, width: '30' }}
+					InputLabelProps=
+					{{
+						shrink: true,
+					}}
+					variant="outlined"
+				/>
             </div>
 			<div>
-				<label>Gun Type</label>
-				<div>
-					<input/>
-				</div>
+				{/* <label>Gun Type</label> */}
+				<TextField
+					label="Gun Type"
+					id=""
+					sx={{ m: 1, width: '30' }}
+					InputLabelProps=
+					{{
+						shrink: true,
+					}}
+					variant="outlined"
+				/>
+            </div>
+			<div>				
+				<TextField
+					label="Incident Beam Energy"
+					id="outlined-start-adornment"
+					sx={{ m: 1, width: '30' }}
+					InputProps=
+					{{
+						endAdornment: <InputAdornment position="end">kg</InputAdornment>,
+					}}
+					InputLabelProps=
+					{{
+						shrink: true,
+					}}
+					variant="outlined"
+				/>
+				
             </div>
 			<div>
-				<label>Incident Beam Energy</label>
-				<div>
-					<input placeholder="in kV"/>
-				</div>
+				<TextField
+					label="Resolution"
+					id="outlined-start-adornment"
+					sx={{ m: 1, width: '30' }}
+					InputProps=
+					{{
+						endAdornment: <InputAdornment position="end">eV</InputAdornment>,
+					}}
+					InputLabelProps=
+					{{
+						shrink: true,
+					}}
+					variant="outlined"
+				/>
             </div>
 			<div>
-				<label>Resolution</label>
-				<div>
-					<input placeholder="in eV"/>
-				</div>
-            </div>
-			<div>
-				<label>Monochromated</label>
-				<div>
-					<select name="monochromated" id="monochromated">
-						<option value="" selected>Select</option>
-						<option value="yes">Yes</option>
-						<option value="no">No</option>
-					</select>
-				</div>
+				<FormControl sx={{ m: 1, width: '25ch' }}>
+					<InputLabel shrink id="demo-simple-select-label">
+						Monochromated
+					</InputLabel>
+					<Select
+						notched
+						labelId="demo-simple-select-label"
+						id="demo-simple-select"
+						// value={age}
+						label="Monochromated"
+						// onChange={handleChange}
+					>
+						<MenuItem value={0}>False</MenuItem>
+						<MenuItem value={1}>True</MenuItem>
+					</Select>
+				</FormControl>
 			</div>
 			<div>
-				<label>Acquisition Mode</label>
-				<div>
-					<select name="monochromated" id="monochromated">
-						<option value="" selected>Select</option>
-						<option value="imagine">Imaging</option>
-						<option value="diffraction">Diffraction</option>
-						<option value="stem">STEM</option>
-						<option value="xas electron yield">XAS Electron Yield</option>
-						<option value="xas transmission">XAS Transmission</option>
-						<option value="fluorescence">Fluorescence</option>
-					</select>
-				</div>
+				<FormControl sx={{ m: 1, width: '25ch' }}>
+					<InputLabel shrink id="demo-simple-select-label">
+						Acquisition Mode
+					</InputLabel>
+					<Select
+						notched
+						labelId="demo-simple-select-label"
+						id="demo-simple-select"
+						// value={age}
+						label="Acquisition Mode"
+						// onChange={handleChange}
+					>
+						<MenuItem value="none">None</MenuItem>
+						<MenuItem value="imagine">Imaging</MenuItem>
+						<MenuItem value="diffraction">Diffraction</MenuItem>
+						<MenuItem value="stem">STEM</MenuItem>
+						<MenuItem value="xas electron yield">XAS Electron Yield</MenuItem>
+						<MenuItem value="xas transmission">XAS Transmission</MenuItem>
+						<MenuItem value="fluorescence">Fluorescence</MenuItem>
+					</Select>
+				</FormControl>
 			</div>
 			<div>
-				<label>Convergence Semi-angle</label>
-				<div>
-					<input placeholder="in mrad"/>
-				</div>
+				<TextField
+					label="Convergence Semi-angle"
+					id="convergence-semi-angle"
+					sx={{ m: 1, width: '30' }}
+					InputProps=
+					{{
+						endAdornment: <InputAdornment position="end">mrad</InputAdornment>,
+					}}
+					InputLabelProps=
+					{{
+						shrink: true,
+					}}
+					variant="outlined"
+				/>
             </div>
 			<div>
-				<label>Collection Semi-angle</label>
-				<div>
-					<input placeholder="in mrad"/>
-				</div>
+				<TextField
+					label="Collection Semi-angle"
+					id="collection-semi-angle"
+					sx={{ m: 1, width: '30' }}
+					InputProps=
+					{{
+						endAdornment: <InputAdornment position="end">mrad</InputAdornment>,
+					}}
+					InputLabelProps=
+					{{
+						shrink: true,
+					}}
+					variant="outlined"
+				/>
             </div>
 			<div>
-				<label>Probe Size</label>
-				<div>
-					<input placeholder="in nm²"/>
-				</div>
+				<TextField
+					label="Probe Size"
+					id="probe-size"
+					sx={{ m: 1, width: '30' }}
+					InputProps=
+					{{
+						endAdornment: <InputAdornment position="end">nm²</InputAdornment>,
+					}}
+					InputLabelProps=
+					{{
+						shrink: true,
+					}}
+					variant="outlined"
+				/>
             </div>
 			<div>
-				<label>Beam Current</label>
-				<div>
-					<input/>
-				</div>
+				<TextField
+					label="Beam Current"
+					id=""
+					sx={{ m: 1, width: '30' }}
+					InputLabelProps=
+					{{
+						shrink: true,
+					}}
+					variant="outlined"
+				/>
             </div>
 			<div>
-				<label>Integration Time</label>
-				<div>
-					<input placeholder="in secs"/>
-				</div>
+				<TextField
+					label="Integration Time"
+					id="integration-time"
+					sx={{ m: 1, width: '30' }}
+					InputProps=
+					{{
+						endAdornment: <InputAdornment position="end">secs</InputAdornment>,
+					}}
+					InputLabelProps=
+					{{
+						shrink: true,
+					}}
+					variant="outlined"
+				/>
             </div>
 			<div>
-				<label>Number of Readouts</label>
-				<div>
-					<input/>
-				</div>
+				<TextField
+					label="Number of Readouts"
+					id=""
+					sx={{ m: 1, width: '30' }}
+					InputLabelProps=
+					{{
+						shrink: true,
+					}}
+					variant="outlined"
+				/>
             </div>
 			<div>
-				<label>Detector</label>
-				<div>
-					<input/>
-				</div>
+				<TextField
+					label="Detector"
+					id=""
+					sx={{ m: 1, width: '30' }}
+					InputLabelProps=
+					{{
+						shrink: true,
+					}}
+					variant="outlined"
+				/>
             </div>
 		</fieldset>
 		<Button type="submit">Submit</Button>
