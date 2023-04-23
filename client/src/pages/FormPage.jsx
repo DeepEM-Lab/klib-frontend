923c5f07b2572203f7231c6b85310bcbf6c3f55a
Guanchen Wu <guanchen2002@gmail.com>
2023-04-23 00:08:43 -0700

    Update FormPage.jsx
    - Applied mui styles to most components except for <fieldset>.

--- client/src/pages/FormPage.jsx
+++ client/src/pages/FormPage.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, FormControl, InputLabel, Grid, MenuItem, Paper, Select, TextField, InputAdornment, Typography} from "@mui/material"
+import { Box, Button, FormControl, FormHelperText, InputLabel, Grid, MenuItem, Paper, Select, TextField, InputAdornment, Typography} from "@mui/material"
 import { useEffect, useState } from "react";
 import PeriodicTable from "../components/PeriodicTable/PeriodicTable"
 import PageParent from "../structures/PageParent"
@@ -47,111 +47,194 @@ const FormPage = () => {
 					i.value = ''
 				))
 			}}>
-                <fieldset>
-                <legend>About You</legend>
-                <div>
-                    <label htmlFor="eelsdb_spectra_author_firstname">Your Name</label>
-                    <div>
-                        <input type="text" name="info" placeholder="First name"/> &nbsp;
-                        <input type="text" name="info" placeholder="Last name"/>
-                    </div>
-                </div>
-                <div>
-                    <label htmlFor="eelsdb_spectra_author_email">Email address</label>
-                    <div>
-                        <input type="email" name="info" placeholder="Your e-mail address"/>
-                        <span><strong>Note:</strong> An account will be created for you if you're not already registered.</span>
-                    </div>
-                </div>
-            	</fieldset>
-             
-
-            <fieldset>
-                <legend>Spectrum Description</legend>
-				<div>
-                    <label htmlFor="post_title">Specimen Name</label>
-                    <div>
-                        <input type="text" name="info" placeholder="Specimen name"/>
-                    </div>
-                </div>
-                <div>
-                    <label htmlFor="eelsdb_spectra_spectrumType">Spectrum Type</label>
-                    <div>
-                        <select name="info">
-                            <option value="coreloss" >Core-loss</option>
-                            <option value="lowloss" >Low-loss</option>
-                            <option value="zeroloss" >Zero-loss</option>
-                            <option value="xrayabs" >XRay Abs</option>
-                        </select>
-                    </div>
-                </div>
-                <div>
-                    <label htmlFor="eelsdb_spectra_spectrumUpload">Data Upload</label>
-                    <div>
-                        <div>
-							<span>
-								<span>
-									Browse&hellip; <input type="file" name="eelsdb_spectra_spectrumUpload" accept=".msa,.dm3,.csv,.txt" size="25"/>
-								</span>
-							</span>
-							
-                        </div>
-                        <span>Files can be in <code>.msa</code>, <code>.csv</code> or <code>.txt</code> format. Files must contain two columns of numbers separated by whitespace or a comma. Lines starting with a <code>#</code> will be ignored.</span>
-                    </div>
-                </div>
-                <div>
-                    <label htmlFor="eelsdb_spectra_spectrumFormula">Specimen Formula</label>
-                    <div>
-                        <input type="text" name="info" placeholder="Specimen Formula"/>
-                    </div>
-                </div>
-                <div>
-                    <label htmlFor="eelsdb_spectra_source_purity">Source and Purity</label>
-                    <div>
-                        <input type="text" name="info" placeholder="Source and Purity"/>
-                    </div>
-                </div>
-                <div>
-                    <label htmlFor="eelsdb_spectra_spectrumKeywords">Associated Keywords</label>
-                    <div>
-                        <input type="text" name="info" placeholder="Separate keywords with commas"/>
-                    </div>
-                </div>
-                <div>
-                    <label htmlFor="eelsdb_spectra_spectrumComments">Author Comments</label>
-                    <div>
-                        <textarea name="info" placeholder="Author Comments"></textarea>
-                    </div>
-                </div>
-            </fieldset>
-
 			<fieldset>
-			<Box>Add Spectra Edge: </Box> 
-			<Select
-				labelId="demo-simple-select-label"
-				id="demo-simple-select"
-				name='info'
-				value={selectedOption}
-				onChange={handleOptionChange}
-			>
-				{data.map((option) => (
-					
-					<MenuItem key={tempStr(option)} value={tempStr(option)}>
-						{tempStr(option)}
-					</MenuItem>
-				))}
-				
-			</Select>
+			<legend>About You</legend>
+			<div>
+				<div>
+				<TextField
+					label="First Name"
+					id=""
+					sx={{ m: 1, width: '15vw' }}//minWidth: '30vw', maxWidth: '30vw'
+					InputLabelProps=
+					{{
+						shrink: true,
+					}}
+					variant="outlined"
+					name='info'
+				/>
+				<TextField
+					label="Last Name"
+					id=""
+					sx={{ m: 1, width: '15vw' }}
+					InputLabelProps=
+					{{
+						shrink: true,
+					}}
+					variant="outlined"
+					name='info'
+				/>
+				</div>
+			</div>
+			<div>
+				<div>
+				<TextField
+					label="Email Address"
+					id=""
+					sx={{ m: 1, width:'30vw' }}
+					InputLabelProps=
+					{{
+						shrink: true,
+					}}
+					variant="outlined"
+					name='info'
+				/>					
+				</div>
+			</div>
 			</fieldset>
+			
+
+		<fieldset>
+			<legend>Spectrum Description</legend>
+			<div>
+				<TextField
+					label="Specimen Name"
+					id=""
+					sx={{ m: 1, width:'30vw' }}
+					InputLabelProps=
+					{{
+						shrink: true,
+					}}
+					variant="outlined"
+					name='info'
+				/>		
+			</div>
+			<div>
+				<FormControl sx={{ m: 1, width: '30vw' }}>
+					<InputLabel shrink id="demo-simple-select-label">
+						Spectrum Type
+					</InputLabel>
+					<Select
+						notched
+						labelId="demo-simple-select-label"
+						id="demo-simple-select"
+						// value={age}
+						label="Acquisition Mode"
+						// onChange={handleChange}
+						name='info'
+					>
+						<MenuItem value="coreloss">Core-loss</MenuItem>
+						<MenuItem value="lowloss">lowloss</MenuItem>
+						<MenuItem value="zeroloss">zeroloss</MenuItem>
+						<MenuItem value="xrayabs">xrayabs</MenuItem>
+						<MenuItem value="xas transmission">XAS Transmission</MenuItem>
+						
+					</Select>
+				</FormControl>
+			</div>
+			<div>
+				<FormControl 
+					sx={{m:1}}
+				>
+					<label>Data Upload</label>
+					<div>
+						<Button
+							variant="contained"
+							component="label"
+						>
+							Choose File
+							<input
+								type="file"
+								accept=".msa,.dm3,.csv,.txt"
+								hidden
+							/>
+						</Button>
+						<FormHelperText>
+						Files can be in <code>.msa</code>, <code>.csv</code> or <code>.txt</code> format. Files must contain two columns of numbers separated by whitespace or a comma. Lines starting with a <code>#</code> will be ignored.
+						</FormHelperText>
+					</div>
+				</FormControl>
+			</div>
+			<div>
+				<TextField
+					label="Specimen Formula"
+					id=""
+					sx={{ m: 1, width: '30vw' }}
+					InputLabelProps=
+					{{
+						shrink: true,
+					}}
+					variant="outlined"
+					name='info'
+				/>			
+			</div>
+			<div>
+				<TextField
+					label="Source and Purity"
+					id=""
+					sx={{ m: 1, width: '30vw' }}
+					InputLabelProps=
+					{{
+						shrink: true,
+					}}
+					variant="outlined"
+					name='info'
+				/>						
+			</div>
+			<div>
+				<TextField
+					label="Associated Keywords"
+					id=""
+					sx={{ m: 1, width: '30vw' }}
+					InputLabelProps=
+					{{
+						shrink: true,
+					}}
+					variant="outlined"
+					name='info'
+				/>		
+			</div>
+			<div>
+				<TextField
+					label="Author Comments"
+					id=""
+					sx={{ m: 1, width: '30vw' }}
+					multiline
+					InputLabelProps=
+					{{
+						shrink: true,
+					}}
+					variant="outlined"
+					name='info'
+				/>					
+			</div>
+		</fieldset>
+
+		<fieldset>
+		<Box>Add Spectra Edge: </Box> 
+		<Select
+			labelId="demo-simple-select-label"
+			id="demo-simple-select"
+			name='info'
+			value={selectedOption}
+			onChange={handleOptionChange}
+		>
+			{data.map((option) => (
+				
+				<MenuItem key={tempStr(option)} value={tempStr(option)}>
+					{tempStr(option)}
+				</MenuItem>
+			))}
+			
+		</Select>
+		</fieldset>
 
 		<fieldset>
 			<legend>Microscope Acquisition Details</legend>
-			{/* <input type="hidden" id="eelsdb_spectra_upload_nonce" name="eelsdb_spectra_upload_nonce" value="86deac9ca7" /><input type="hidden" name="_wp_http_referer" value="/submit-data/" />			 */}
 			<div>
 				<TextField
 					label="Microscope Name/Model"
 					id=""
-					sx={{ m: 1, width: '30' }}
+					sx={{ m: 1, width: '30vw' }}
 					InputLabelProps=
 					{{
 						shrink: true,
@@ -159,13 +242,12 @@ const FormPage = () => {
 					variant="outlined"
 					name='info'
 				/>
-            </div>
+			</div>
 			<div>
-				{/* <label>Gun Type</label> */}
 				<TextField
 					label="Gun Type"
 					id=""
-					sx={{ m: 1, width: '30' }}
+					sx={{ m: 1, width: '30vw' }}
 					InputLabelProps=
 					{{
 						shrink: true,
@@ -173,12 +255,12 @@ const FormPage = () => {
 					variant="outlined"
 					name='info'
 				/>
-            </div>
+			</div>
 			<div>				
 				<TextField
 					label="Incident Beam Energy"
 					id="outlined-start-adornment"
-					sx={{ m: 1, width: '30' }}
+					sx={{ m: 1, width: '30vw' }}
 					InputProps=
 					{{
 						endAdornment: <InputAdornment position="end">kg</InputAdornment>,
@@ -191,12 +273,12 @@ const FormPage = () => {
 					name='info'
 				/>
 				
-            </div>
+			</div>
 			<div>
 				<TextField
 					label="Resolution"
 					id="outlined-start-adornment"
-					sx={{ m: 1, width: '30' }}
+					sx={{ m: 1, width: '30vw' }}
 					InputProps=
 					{{
 						endAdornment: <InputAdornment position="end">eV</InputAdornment>,
@@ -208,9 +290,9 @@ const FormPage = () => {
 					variant="outlined"
 					name='info'
 				/>
-            </div>
+			</div>
 			<div>
-				<FormControl sx={{ m: 1, width: '25ch' }}>
+				<FormControl sx={{ m: 1, width: '30vw' }}>
 					<InputLabel shrink id="demo-simple-select-label">
 						Monochromated
 					</InputLabel>
@@ -229,7 +311,7 @@ const FormPage = () => {
 				</FormControl>
 			</div>
 			<div>
-				<FormControl sx={{ m: 1, width: '25ch' }}>
+				<FormControl sx={{ m: 1, width: '30vw' }}>
 					<InputLabel shrink id="demo-simple-select-label">
 						Acquisition Mode
 					</InputLabel>
@@ -242,7 +324,7 @@ const FormPage = () => {
 						// onChange={handleChange}
 						name='info'
 					>
-						<MenuItem value="none">None</MenuItem>
+						{/* <MenuItem value="none">None</MenuItem> */}
 						<MenuItem value="imagine">Imaging</MenuItem>
 						<MenuItem value="diffraction">Diffraction</MenuItem>
 						<MenuItem value="stem">STEM</MenuItem>
@@ -256,7 +338,7 @@ const FormPage = () => {
 				<TextField
 					label="Convergence Semi-angle"
 					id="convergence-semi-angle"
-					sx={{ m: 1, width: '30' }}
+					sx={{ m: 1, width: '30vw' }}
 					InputProps=
 					{{
 						endAdornment: <InputAdornment position="end">mrad</InputAdornment>,
@@ -268,12 +350,12 @@ const FormPage = () => {
 					variant="outlined"
 					name='info'
 				/>
-            </div>
+			</div>
 			<div>
 				<TextField
 					label="Collection Semi-angle"
 					id="collection-semi-angle"
-					sx={{ m: 1, width: '30' }}
+					sx={{ m: 1, width: '30vw' }}
 					InputProps=
 					{{
 						endAdornment: <InputAdornment position="end">mrad</InputAdornment>,
@@ -285,12 +367,12 @@ const FormPage = () => {
 					variant="outlined"
 					name='info'
 				/>
-            </div>
+			</div>
 			<div>
 				<TextField
 					label="Probe Size"
 					id="probe-size"
-					sx={{ m: 1, width: '30' }}
+					sx={{ m: 1, width: '30vw' }}
 					InputProps=
 					{{
 						endAdornment: <InputAdornment position="end">nm²</InputAdornment>,
@@ -302,12 +384,12 @@ const FormPage = () => {
 					variant="outlined"
 					name='info'
 				/>
-            </div>
+			</div>
 			<div>
 				<TextField
 					label="Beam Current"
 					id=""
-					sx={{ m: 1, width: '30' }}
+					sx={{ m: 1, width: '30vw' }}
 					InputLabelProps=
 					{{
 						shrink: true,
@@ -315,12 +397,12 @@ const FormPage = () => {
 					variant="outlined"
 					name='info'
 				/>
-            </div>
+			</div>
 			<div>
 				<TextField
 					label="Integration Time"
 					id="integration-time"
-					sx={{ m: 1, width: '30' }}
+					sx={{ m: 1, width: '30vw' }}
 					InputProps=
 					{{
 						endAdornment: <InputAdornment position="end">secs</InputAdornment>,
@@ -332,12 +414,12 @@ const FormPage = () => {
 					variant="outlined"
 					name='info'
 				/>
-            </div>
+			</div>
 			<div>
 				<TextField
 					label="Number of Readouts"
 					id=""
-					sx={{ m: 1, width: '30' }}
+					sx={{ m: 1, width: '30vw' }}
 					InputLabelProps=
 					{{
 						shrink: true,
@@ -345,12 +427,12 @@ const FormPage = () => {
 					variant="outlined"
 					name='info'
 				/>
-            </div>
+			</div>
 			<div>
 				<TextField
 					label="Detector"
 					id=""
-					sx={{ m: 1, width: '30' }}
+					sx={{ m: 1, width: '30vw' }}
 					InputLabelProps=
 					{{
 						shrink: true,
@@ -358,9 +440,9 @@ const FormPage = () => {
 					variant="outlined"
 					name='info'
 				/>
-            </div>
+			</div>
 		</fieldset>
-		<Button type="submit">Submit</Button>
+		<Button type="submit" variant="contained">Submit</Button>
 		<div>{textInfo}</div>
 	</form>
     </PageParent>
