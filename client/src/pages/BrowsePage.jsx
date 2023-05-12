74434cb64ea90d116636b852384845c8d81cb426
Guanchen Wu <guanchen2002@gmail.com>
2023-05-11 23:39:28 -0700

    Updated Browse Page
    - Updated table fields.
    - Inserted file names from public/fake_files/

--- client/src/pages/BrowsePage.jsx
+++ client/src/pages/BrowsePage.jsx
@@ -2,7 +2,27 @@
 import { DataGrid } from "@mui/x-data-grid"
 import PageParent from "../structures/PageParent"
 
+// import data from "/fake_files/"
+
+// var fs = require('fs');
+// var files = fs.readdirSync('/../../public/fake_files/');
+function importAll(r) {
+    return r.keys().map(r);
+}
+//, false, /\.(txt)$/
+const fileNames = importAll(require.context('../../public/fake_files/'));
+
+function getRows(f, r){
+    for (let i = 0; i < f.length; i++) {
+        var temp = f[i].split('/')[3].split('.')[0];
+        console.log(temp)
+        r.push({ id: i, spectrumTitle: temp});//field: fileNames[i].default.name, headerName: fileNames[})
+    }
+}
+
+
 const columns = [
+    // { field: 'temp', headerName: 'TEMP', width: 70 },
     // { field: 'id', headerName: 'ID', width: 70 },
     // { field: 'firstName', headerName: 'First name', width: 130 },
     // { field: 'lastName', headerName: 'Last name', width: 130 },
@@ -25,23 +45,40 @@ const columns = [
     {
         field: 'spectrumTitle', 
         headerName: 'Spectrum Title', 
-        width: 'auto'
+        width: 300
     },
     {
         field: 'formula', 
         headerName: 'Formula', 
-        width: 'auto'
+        width: 150
     },
     {
         field: 'edge', 
         headerName: 'Edge', 
-        width: 'auto'
+        width: 100
     },
     {
         field: 'minEnergy', 
         headerName: 'Min Energy', 
-        width: 'auto'
+        width: 150
     },
+    {
+        field: 'maxEnergy', 
+        headerName: 'Max Energy', 
+        width: 150
+    },
+    {
+        field: 'resolution', 
+        headerName: 'Resolution', 
+        width: 150
+    },
+    {
+        field: 'spectrumType', 
+        headerName: 'Spectrum Type', 
+        width: 150
+    }
+
+
     // {
     //     field: 'maxEnergy', 
     //     headerName: 'Max Energy', 
@@ -64,27 +101,53 @@ const columns = [
     // },
 
   ];
-  
+
+
 const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    // { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+    // { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+    // { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+    // { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+    // { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+    // { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+    // { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+    // { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+    // { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+
+    // { id: 1, spectrumTitle: '1', formula: '1', edge: '1', minEnergy: '1', maxEnergy: '1', resolution:'1', spectrumType: '1' },
+    // {spectrumTitle: '1', formula: '1', edge: '1', minEnergy: '1', maxEnergy: '1', resolution:'1', spectrumType: '1'},
+    // {spectrumTitle: '1', formula: '1', edge: '1', minEnergy: '1', maxEnergy: '1', resolution:'1', spectrumType: '1'},
+    // {spectrumTitle: '1', formula: '1', edge: '1', minEnergy: '1', maxEnergy: '1', resolution:'1', spectrumType: '1'},
+    // {spectrumTitle: '1', formula: '1', edge: '1', minEnergy: '1', maxEnergy: '1', resolution:'1', spectrumType: '1'},
+    // {spectrumTitle: '1', formula: '1', edge: '1', minEnergy: '1', maxEnergy: '1', resolution:'1', spectrumType: '1'},
+    // {spectrumTitle: '1', formula: '1', edge: '1', minEnergy: '1', maxEnergy: '1', resolution:'1', spectrumType: '1'},
+    // {spectrumTitle: '1', formula: '1', edge: '1', minEnergy: '1', maxEnergy: '1', resolution:'1', spectrumType: '1'},
+    // {spectrumTitle: '1', formula: '1', edge: '1', minEnergy: '1', maxEnergy: '1', resolution:'1', spectrumType: '1'},
+    // {spectrumTitle: '1', formula: '1', edge: '1', minEnergy: '1', maxEnergy: '1', resolution:'1', spectrumType: '1'},
+
 ];
+
+getRows(fileNames, rows);
 
 const BrowsePage = () => {
     return (
         <PageParent>
-            <div style={{ height: 400, width: '100%' }}>
+            {/* <div>
+                {{fileNames}}
+            </div> */}
+            <div style={{ height: 'auto', width: '100%' }}>
                 <DataGrid
                     rows={rows}
                     columns={columns}
-                    paginationModel={{ page: 0, pageSize: 5 }}
+                    initialState={{
+                        pagination: {
+                          paginationModel: {
+                            pageSize: 50,
+                          },
+                        },
+                    }}
+                    pageSizeOptions={[5]}
+                    disableRowSelectionOnClick
                     // checkboxSelection
                 />
             </div>
